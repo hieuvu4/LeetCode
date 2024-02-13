21d808dcc8356c465120699677757049e76b1df9
Hieu <ndhieuv@gmx.net>
2024-02-13 14:53:50 +0100

    added solution max profit

--- src/main/java/medium/bestTimeToBuyAndSellStocks/Solution.java
+++ src/main/java/medium/bestTimeToBuyAndSellStocks/Solution.java
@@ -8,7 +8,10 @@ public class Solution {
     }
 
     public static int maxProfit(int[] prices) {
-        // TODO
-        return -1;        
+        int profit = 0;
+        for(int i = 1; i < prices.length; i++) {
+            profit += (prices[i] > prices[i-1])? prices[i] - prices[i-1]: 0;
+        }
+        return profit;
     }
 }
