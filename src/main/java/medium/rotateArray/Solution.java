713c006ad4e50a99ec0f2f7beaeb48e7eb296d9c
Hieu <ndhieuv@gmx.net>
2024-02-14 11:35:33 +0100

    upgraded rotate array problem

--- src/main/java/medium/rotateArray/Solution.java
+++ src/main/java/medium/rotateArray/Solution.java
@@ -9,8 +9,32 @@ public class Solution {
         rotate(nums, k);
     }
 
-    // Test Failed: Time Limit Exceeded
+
     public static void rotate(int[] nums, int k) {
+        // for the case if k negative -> -1 == 6
+        // and no "endless" rotation because for arr.length = 7 -> 7 = 14 = 21 = etc.
+        k = k % nums.length;
+        k += (k < 0)? nums.length: 0;
+        rotate(nums, 0, nums.length-k-1);
+        rotate(nums, nums.length-k, nums.length-1);
+        rotate(nums, 0, nums.length-1);
+    }
+
+    private static void rotate(int[] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    
+
+
+    
+    // Test Failed: Time Limit Exceeded
+    public static void rotate2(int[] nums, int k) {
         for(int i = 0; i < k; i++) {
             int temp = nums[nums.length-1];
             for(int j = nums.length-1; j > 0; j--) {
