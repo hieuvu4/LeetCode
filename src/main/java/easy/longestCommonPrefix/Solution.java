0c9d8ffc79c64df524e0817da359f6fac0a1913e
Hieu <ndhieuv@gmx.net>
2024-02-15 10:45:45 +0100

    added solution for longest common prefix problem

--- src/main/java/easy/longestCommonPrefix/Solution.java
+++ src/main/java/easy/longestCommonPrefix/Solution.java
@@ -1,5 +1,7 @@
 package easy.longestCommonPrefix;
 
+import java.util.stream.Stream;
+
 public class Solution {
     
     public static void main(String[] args) {
@@ -10,19 +12,19 @@ public class Solution {
 
 
     public static String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
+        if(strs == null) return "";
+        int minSize = Integer.MAX_VALUE;
+        for(String s: strs) minSize = Math.min(minSize, s.length());
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i = 0; i < minSize; i++) {
+            for(int j = 1; j < strs.length; j++) {
+                if(strs[j].charAt(i) != strs[0].charAt(i)) return stringBuilder.toString();
+            }
+            stringBuilder.append(strs[0].charAt(i));
         }
 
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) {
-                    return "";
-                }
-            }
-        }
-        return prefix;
+        return stringBuilder.toString();
     }
 }
