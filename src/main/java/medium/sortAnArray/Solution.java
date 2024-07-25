0bd7801c202f425cf88e76021fb5e58764cdf86b
Hieu <ndhieuv@gmx.net>
2024-07-25 13:15:32 +0200

    added sort an array problem and solution

---
+++ src/main/java/medium/sortAnArray/Solution.java
@@ -0,0 +1,49 @@
+package medium.sortAnArray;
+
+import java.util.Arrays;
+
+public class Solution {
+    
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums);
+        return nums;
+    }
+
+    public void mergeSort(int[] array){
+        int n = array.length;
+        if(n <= 1) return;
+
+        int mid = n / 2;
+        int[] leftArray = Arrays.copyOfRange(array, 0, mid);
+        int[] rightArray = Arrays.copyOfRange(array, mid, n);
+
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        mergeSortHelper(leftArray, rightArray, array);
+    }
+
+    public void mergeSortHelper(int[] leftArray, int[] rightArray, int[] array){
+        int leftSize = leftArray.length;
+        int rightSize = rightArray.length;
+        int i = 0, j = 0, k = 0;
+
+        // Merge the two subarrays into the original array
+        while (i < leftSize && j < rightSize) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k++] = leftArray[i++];
+            } else {
+                array[k++] = rightArray[j++];
+            }
+        }
+
+        // Copy the remaining elements of leftArray, if any
+        while (i < leftSize) {
+            array[k++] = leftArray[i++];
+        }
+
+        // Copy the remaining elements of rightArray, if any
+        while (j < rightSize) {
+            array[k++] = rightArray[j++];
+        }
+    } 
+}
