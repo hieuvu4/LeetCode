ceb64a0ac9bf4b60bd36a7370f37d48f62bc7219
Hieu <ndhieuv@gmx.net>
2024-08-15 14:45:05 +0200

    added lemonade change problem and solution

---
+++ src/main/java/easy/lemonadeChange/Solution.java
@@ -0,0 +1,36 @@
+package easy.lemonadeChange;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five_dollars = 0, ten_dollars = 0;
+
+        for (int x : bills) {
+            switch (x) {
+                case 5:
+                    five_dollars++;
+                    break;
+                case 10:
+                    if (five_dollars > 0) {
+                        five_dollars--;
+                        ten_dollars++;
+                    } else {
+                        return false;
+                    }   break;
+                default:
+                    if (five_dollars > 0 && ten_dollars > 0) {
+                        five_dollars--;
+                        ten_dollars--;
+                    } else if (five_dollars > 2) {
+                        five_dollars -= 3;
+                    } else {
+                        return false;
+                    }   break;
+            }
+        }
+
+        return true;
+    }
+}
